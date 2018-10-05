cfa4a38fae4beff40619bdc2f7b72e3e0daa9f8c
Alexandre Morcrette <clavos@hotmail.fr>
2018-10-05 11:17:02 +0200

    Création des routes urls

--- grimoire/views.py
+++ grimoire/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Page Grimoire que l'utilisateur atteint quand il souhaite consulter ses deck et cartes")
